557c93f33db61b9c5a871f38c64ef4aef90b45a1
piaip <piaip2010@hotmail.com>
2017-07-12 17:31:36 +0800

    Update Warmup-2.java

--- Warmup-2.java
+++ Warmup-2.java
@@ -43,3 +43,72 @@ public String stringSplosion(String str) {
   return temp + str;
 }
 
+//Warmup-2 > last2 
+public int last2(String str) {
+  int len = str.length();
+  int count = 0;
+  String last = "";
+  if(len < 2) return 0;
+  last = str.substring(len-2, len);
+  for(int i=0; i<len-2; i++){
+    if(str.substring(i, i+2).equals(last)) count += 1;
+  }
+  return count;
+}
+
+//Warmup-2 > arrayCount9 
+public int arrayCount9(int[] nums) {
+  int count = 0;
+  for(int i=0; i<nums.length; i++){
+    if(nums[i] == 9) count += 1;
+  }
+  return count;
+}
+
+//Warmup-2 > arrayFront9
+public boolean arrayFront9(int[] nums) {
+  int len = nums.length;
+  for(int i=0; i<len; i++){
+    if(i >= 4) return false;
+    if(nums[i] == 9) return true;
+  }
+  return false;
+}
+
+//Warmup-2 > array123
+public boolean array123(int[] nums) {
+  int len = nums.length;
+  if(len < 3) return false;
+  for(int i=0; i<nums.length-2; i++){
+    if(nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3) return true;
+  }
+  return false;
+}
+
+//Warmup-2 > stringMatch
+public int stringMatch(String a, String b) {
+    int min = Math.min(a.length(), b.length());
+    int count = 0;
+    
+    for(int i=0; i<min-1; i++){
+      if(a.substring(i, i+2).equals(b.substring(i, i+2))) count += 1;
+    }
+    return count;
+}
+
+//Warmup-2 > stringX 
+public String stringX(String str) {
+  int len = str.length();
+  
+  if(len < 2) return str;
+  
+  int midlen = 0;
+  String temp = "";
+  String first = str.substring(0, 1);
+  String last = str.substring(len-1);
+  String mid = str.substring(1, len-1);
+  for(int i=0; i<mid.length(); i++){
+    if(!mid.substring(i, i+1).equals("x")) temp += mid.substring(i, i+1);
+  }
+  return first + temp + last;
+}
