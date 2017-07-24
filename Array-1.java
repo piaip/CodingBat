6b2c296719d7b07fd8873e940d12ff4685f2554a
piaip <piaip2010@hotmail.com>
2017-07-24 16:08:35 +0800

    Update and rename Array-1 to Array-1.java

--- Array-1.java
+++ Array-1.java
@@ -195,3 +195,12 @@ public int[] make2(int[] a, int[] b) {
   }
   return myarr;
 }
+
+//Array-1 > front11 
+public int[] front11(int[] a, int[] b) {
+  if(a.length == 0 && b.length == 0)  return a;
+  if(a.length == 0 && b.length > 0)   return new int[] {b[0]}; 
+  if(a.length > 0 && b.length == 0)   return new int[] {a[0]};
+  return new int[] {a[0], b[0]};
+}
+
